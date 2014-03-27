dc98a7468d3e0c59c54ced1e8d863309c3456c2b
Jaroslaw Waliszko <jaroslaw.waliszko@gmail.com>
2014-03-27 20:10:59 +0100

    Regex entries removal supplemented + minor renamings.

--- other/del_resurrect_from_registry.bat
+++ other/del_resurrect_from_registry.bat
@@ -1,3 +1,5 @@
+REG DELETE HKEY_CURRENT_USER\Software\Microsoft\VisualStudio\10.0Exp\Resurrect /f
+REG DELETE HKEY_CURRENT_USER\Software\Microsoft\VisualStudio\10.0\Resurrect /f
 REG DELETE HKEY_CURRENT_USER\Software\Microsoft\VisualStudio\11.0Exp\Resurrect /f
 REG DELETE HKEY_CURRENT_USER\Software\Microsoft\VisualStudio\11.0\Resurrect /f
 REG DELETE HKEY_CURRENT_USER\Software\Microsoft\VisualStudio\12.0Exp\Resurrect /f
