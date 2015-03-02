72540db688d94cc494fc4cdd2213af3834ed56a7
Jaroslaw Waliszko <jaroslaw.waliszko@gmail.com>
2015-03-02 21:39:52 +0100

    Supported runtime version + few other strings fixed.

--- other/del_resurrect_from_registry.bat
+++ other/del_resurrect_from_registry.bat
@@ -4,3 +4,5 @@ REG DELETE HKEY_CURRENT_USER\Software\Microsoft\VisualStudio\11.0Exp\Resurrect /
 REG DELETE HKEY_CURRENT_USER\Software\Microsoft\VisualStudio\11.0\Resurrect /f
 REG DELETE HKEY_CURRENT_USER\Software\Microsoft\VisualStudio\12.0Exp\Resurrect /f
 REG DELETE HKEY_CURRENT_USER\Software\Microsoft\VisualStudio\12.0\Resurrect /f
+REG DELETE HKEY_CURRENT_USER\Software\Microsoft\VisualStudio\14.0Exp\Resurrect /f
+REG DELETE HKEY_CURRENT_USER\Software\Microsoft\VisualStudio\14.0\Resurrect /f
